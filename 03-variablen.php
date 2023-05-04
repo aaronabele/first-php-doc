262f1e24f65e8d9b561adbd88f68bbd898422adb
Aaron Abele <aaronabele.coding@gmail.com>
2023-05-04 19:33:24 +0200

    adds vars doc

---
+++ 03-variablen.php
@@ -0,0 +1,8 @@
+<?php
+
+$greeting = "Hello Mars"; //Variablen werden immer mit $xx deklariert
+
+echo "Hallo Welt";
+echo "/n"; //Zeilenumbruch (Quelltext), muss in Anführungzeichen geschrieben werden
+echo "<br>"; //HTML Zeilenumbruch
+echo $greeting;
